6a06aba7e0ed1e3ef6eb6e23cf30fad08f2137cc
Tom van Kouterik <Tomvankouterik2005@gmail.com>
2026-04-23 11:12:34 +0200

    safety commit

---
+++ Assets/Ash/Scripts/CustomerManager.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> seats = new List<GameObject>();
+    [SerializeField] private List<GameObject> plates = new List<GameObject>();
+    [SerializeField] private GameObject Customer;
+
+
+}
