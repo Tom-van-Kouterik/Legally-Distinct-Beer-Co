1d82ed32711ba429b34bc1e78faeb9b9d23757ca
Tom van Kouterik <Tomvankouterik2005@gmail.com>
2026-04-23 11:46:41 +0200

    end of school day push

--- Assets/Ash/Scripts/CustomerManager.cs
+++ Assets/Ash/Scripts/CustomerManager.cs
@@ -7,5 +7,18 @@ public class CustomerManager : MonoBehaviour
     [SerializeField] private List<GameObject> plates = new List<GameObject>();
     [SerializeField] private GameObject Customer;
 
+    public void SpawnCustomer()
+    {
+        //spawn the customer prefab and link it to the seat and plate of the asosiated spot and mark this spot as "taken"
+    }
 
+    public void DestroyCustomer()
+    {
+        //destroy the corosponding customer and mark their spot as "open"
+    }
+
+    public void CompleteOrder()
+    {
+        //either add or remove gold from gold total
+    }
 }
